8e1b644ac4ffe6fe6bfdece4f73e078ec65a1016
Vishnu9804 <burkhawalavishnu@gmail.com>
2025-02-25 20:33:37 +0530

    Initial Commit

---
+++ Backend/src/test/java/com/example/ShelfScrap/ShelfScrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShelfScrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfScrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
